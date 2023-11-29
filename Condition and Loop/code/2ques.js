92429147e21847ee4c9ff576814a6f20e7440dc1
Amit <amiamit576@gmail.com>
2023-11-29 11:22:05 +0530

    add condtional and loop assg

---
+++ Condition and Loop/code/2ques.js
@@ -0,0 +1,12 @@
+let marks = 95;
+if (marks > 90) {
+    console.log("the student  get grade A")
+} else if(marks > 70 && marks <= 90) {
+    console.log("the student  get grade B")
+}
+else if (marks > 50 && marks <= 70) {
+    console.log("the student  get grade C") 
+}
+else {
+     console.log("the student  get grade F") 
+}
---
+++ Condition and Loop/code/5ques.js
@@ -0,0 +1,14 @@
+// while loop
+// asecnding order
+let n = 1;
+while (n <=25) {
+    console.log(n)
+    n++;
+    
+}
+// desecending
+let m = 25;
+while (m >= 1) {
+    console.log(m)
+    m--
+}
